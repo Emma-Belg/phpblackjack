4b3449349dee9541374713da98294319384a90ad
emma <emma.wishart@live.com.au>
2020-02-13 15:41:34 +0100

    getting the first deal, having problems converting var to ints

---
+++ game.php
@@ -0,0 +1,58 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require 'index.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <title>Blackjack</title>
+</head>
+<body>
+<div class="container">
+    <h1>Blackjack Game</h1>
+    <nav>
+    </nav>
+    <form method="post">
+
+
+        <fieldset>
+            <legend>Player</legend>
+
+            <div><?php
+                echo $player->set_hit();
+                ?></div>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Dealer</legend>
+        </fieldset>
+        <button name = "deal" type="submit" value="0" class="btn btn-info">Deal!</button>
+        <button name = "hit" type="submit" value="1" class="btn btn-info">Hit Me!</button>
+        <button name = "stand" type="submit" value="2" class="btn btn-info">Stand</button>
+        <button name = "surrender" type="submit" value="3" class="btn btn-info">Surrender</button>
+    </form>
+
+    <footer>
+        <br>
+        <br>
+        Thank you for playing.</footer>
+    <div><?php
+        whatIsHappening();
+        ?></div>
+    <br>
+    <br>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+session_start();
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+//whatIsHappening();
+
+class Blackjack
+{
+
+    public $score;
+    public $newcard;
+    public $firstcard;
+    public $secondcard;
+    public $cardsonTable = array();
+    public $minCard = 1;
+    public $maxCard = 11;
+
+
+    public function set_firstDeal(){
+        if(isset($_POST["deal"])){
+            $this->firstcard = rand($this->minCard, $this->maxCard);
+            $this->secondcard = rand($this->minCard, $this->maxCard);
+            array_push($this->cardsonTable, $this->firstcard, $this->secondcard);
+            echo "You were dealt a ".implode(" and ", $this->cardsonTable)."<br>";
+            echo "Your total is:". intval($this->firstcard). +intval($this->secondcard)."<br>";
+        }
+    }
+    public function set_hit()
+    {
+        if (isset($_POST["hit"])) {
+            $this->newcard = rand($this->minCard, $this->maxCard);
+
+            do {
+                $NewScore = $this->score + $this->newcard;
+            } while ($this->score < 21);
+            array_push($this->cardsonTable, $NewScore);
+
+            echo $this->newcard . "<br>";
+            echo $NewScore;
+            return $this->cardsonTable;
+        }
+    }
+
+
+    public function get_score($newcard)
+    {
+        $score = $this->score + $newcard;
+        echo $score;
+    }
+
+ /*   public function hit()
+    {
+        if (!empty($_POST["hit"])) {
+            $newcard = $_POST["hit"];
+            $addingScore = array();
+
+            foreach ($newcard as $key => $amount) {
+                $prodPrice = $products[$key]['price'];
+                $addingPrice = $prodPrice * $amount;
+                array_push($addingScore, $addingPrice);
+            }
+            $total = array_sum($addingScore);
+            $totalValue = $total;
+            if (!isset($_COOKIE["card"])) {
+                setcookie("card", strval($totalValue));
+            } else {
+                $totalValue = $totalValue + $_COOKIE["card"];
+                setcookie("card", strval($totalValue));
+            }
+            $totalValue = $_COOKIE["card"];
+        } else {
+            $totalValue = 0;
+        }
+    }*/
+
+
+    /*    public function stand($score){
+            $score = $this->score;
+        }*/
+
+
+}
+
+$player = new Blackjack();
+$player->set_firstDeal();
+$player->set_hit();
+$player->set_hit();
+$player->get_score($player->set_hit());
+
+$dealer = new Blackjack();
+
+
